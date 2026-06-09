bc80eb37a5b0ef4cb993b33fa5cb7f64e08cf4a9
FacelessOne4 <ryandesam@gmail.com>
2026-06-08 21:05:30 -0300

    Commit

--- glossario.js
+++ glossario.js
@@ -1,15 +1,32 @@
+//Pegar elemento (TAG) pelo ID
 document.getElementById('num1');
+ 
+//Pegar elemento (valor) pelo ID e colocar numa variável
 let numero = document.getElementById('num1').value;
-document.getElementById('num1').value=30;
+ 
+//Atribuir outro valor ao elemento pelo id
+document.getElementById('num1').value = 30;
+ 
+ 
+//Pegar elemento pela tag
 document.getElementsByTagName('p');
-document.getElementsByTagName('p');
-document.getElementsByTagName('p')[0];
-document.getElementsByTagName('p')[0].innerHTML;
-
-document.querySelector('#num1');
+ 
+//Pegar um array de tags pelo nome da tag
+document.getElementsByTagName('p'); //array
+ 
+//Pegar um item do array de tags pelo nome da tag
+document.getElementsByTagName('p')[0] //seleciono a tag
+ 
+//Pegar o conteúdo de uma tag específica dentro do array
+document.getElementsByTagName('p')[0].innerHTML; //seleciono o conteúdo
+ 
+// Pego um unico elemento com class, tag ou id. OU. o primeiro que encontrar.
+document.querySelector('p');
 document.querySelector('.box');
-document.querySelector('p')
-
-document.querySelectorAll('#num1');
+document.querySelector('#num1');
+ 
+// Pego um array de elementos com class, tag ou id.
+document.querySelectorAll('p');
 document.querySelectorAll('.box');
-document.querySelectorAll('p')
+document.querySelectorAll('#num1');[1];
+ 
--- main.js
+++ main.js
@@ -1,13 +1,35 @@
+const botao = document.getElementById("#btnCalcular")
+const numero1 = document.querySelector("#num1")
+const numero2 = document.querySelector("#num2")
+const resultado = document.querySelector("#resultado")
+
 function somar(){
-let numero1, numero2, resultado;
+let num1, num2, resCalculo;
 
-numero1 = document.querySelector('#num1').value;
-numero1 = document.querySelector('#num2').value;
-numero1 = document.querySelector('#resultado').innerHTML;
+num1 = Number(numero1.value);
+num2 = Number(numero2.value);
 
-resultado = numero1 + numero2;
+alert(isNaN )
 
-document.querySelector("#resultado").innerHTML=resultado;
+resCalculo = num1+num2;
+resultado.innerHTML=resCalculo;
 }
 
-somar()
+    function validarEntradas(){
+        if (isNaN(numero1.value) === "" || isNaN(numero2.value) === "") {
+            //não posso calcular
+            resultado.innerHTML = "Valores inválidos. Insira novos valores (números)"
+            return false
+        } else {
+            //posso calcular
+            return ture
+        }
+    }
+
+    function mostrarMensagemErro(){
+        resultado.innerHTML = "Valores inválidos. Insira novos valores (números)";
+    }
+
+//O Programa começa aqui
+if (validarEntradas()==true)
+botao.addEventListener("click", somar)
